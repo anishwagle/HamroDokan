5f90a8d7ad1ba8d42e688762309287317cc2ce41
Anish Wagle <krazyyplayer@users.noreply.github.com>
2019-02-03 19:27:53 +0545

    ok

---
+++ HamroDokan/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using HamroDokan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace HamroDokan.Controllers
+{
+    public class SearchController : Controller
+    {
+        ShoppingStoreEntities storeDB = new ShoppingStoreEntities();
+        // GET: Search
+        public ActionResult Index(string searching)
+        {
+
+            return View(storeDB.Items.Where(x => x.Keyword1.Contains(searching)||searching==null).ToList());
+        }
+    }
+}
