0a2d488df7dc748c51d56762592164515d9ba268
Anish Wagle <krazyyplayer@users.noreply.github.com>
2019-02-03 21:41:07 +0545

    ok

--- HamroDokan/Controllers/SearchController.cs
+++ HamroDokan/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using HamroDokan.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,7 +14,27 @@ namespace HamroDokan.Controllers
         public ActionResult Index(string searching)
         {
 
+
             return View(storeDB.Items.Where(x => x.Keyword1.Contains(searching)||searching==null).ToList());
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            var path = "";
+            if (file!=null)
+            {
+                if(file.ContentLength>0)
+                {
+                    if (Path.GetExtension(file.FileName).ToLower() ==".jpg")
+                    {
+                        path = Path.Combine(Server.MapPath(@"..\..\Content\Images\Unknown"), file.FileName);
+                        file.SaveAs(path);
+
+                    }
+
+                }
+            }
+            return View();
         }
     }
 }
